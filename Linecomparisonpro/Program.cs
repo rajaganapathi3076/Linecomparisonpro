2fce4773f17d27cf0348506189c2b564b33375cf
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-05-14 16:35:45 +0530

    [raja] adding linecom

---
+++ Linecomparisonpro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linecomparisonpro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinecomparisonproUC1");
+            Console.WriteLine("plese Enter the x1 and y1 cordinates");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("plese Enter the x2 and y2 cordinates");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            int distance = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
+            Console.WriteLine("The distance of coordinator : %4F"    + Math.Sqrt(distance));
+            Console.ReadLine();
+
+        }
+    }
+}

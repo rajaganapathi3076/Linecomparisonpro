55818a475d16166225881c0b51007b6df1840e31
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-05-28 12:19:07 +0530

    adding UC4

---
+++ Linecomparisonpro/Linecomparison.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linecomparisonpro
+{
+    internal class Linecomparison
+    {
+        public static void CompareToLength()
+            
+        {
+            Console.WriteLine("Plese enter the value of x1 and y1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Plese enter the value of x2 and y2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Plese enter the value of x3 and y3");
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Plese enter the value of x4 and y4");
+            int x4 = Convert.ToInt32(Console.ReadLine());
+            int y4 = Convert.ToInt32(Console.ReadLine());
+
+            int distance1 = ((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
+            int distance2 = ((x4 - x3) * (x4 - x3)) + ((y4 - y3) * (y4 - y3));
+            Console.WriteLine("The length of the  1st line :%.4F" + Math.Sqrt(distance1));
+            Console.WriteLine("The length of the  2st line :%.4F" + Math.Sqrt(distance2));
+
+            if (distance1 >= distance2)
+            {
+                Console.WriteLine("The 1st line is greater");
+            }
+            else if (distance1 < distance2)
+            {
+                Console.WriteLine("The first number is smallerthen second number");
+            }
+            else
+            {
+                Console.WriteLine("Nill");
+            }
+            Console.ReadLine();
+        }
+        }
+    }
+}
